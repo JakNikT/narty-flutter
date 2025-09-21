8f1accb38cf8c877d23940e6f51dbed1374c8bee
JakNikT <waciak00@gmail.com>
2025-09-21 21:14:52 +0200

    podział programu na foldery

---
+++ interfejs/__init__.py
@@ -0,0 +1,8 @@
+"""
+interfejs - moduł projektu Asystent Doboru Nart
+Zawiera komponenty interfejsu użytkownika
+"""
+
+from interfejs.okno_glowne import SkiApp, DatePickerDialog
+
+__all__ = ['SkiApp', 'DatePickerDialog']
